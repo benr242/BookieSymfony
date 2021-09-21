f118cfaa036eada921484964faf4c9a8f9de9ca1
benr242 <benr242@localhost.localdomain>
2021-09-21 10:30:18 -0400

    controller, working page

---
+++ src/Controller/BookieController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookieController extends AbstractController
+{
+    /**
+     * @Route("/bookie", name="bookie")
+     */
+    public function index(): Response
+    {
+        return $this->render('bookie/index.html.twig', [
+            'controller_name' => 'BookieController',
+        ]);
+    }
+}
